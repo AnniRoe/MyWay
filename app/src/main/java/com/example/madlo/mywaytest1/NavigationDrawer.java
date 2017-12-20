0ca3edcff4ff30ac71ee80f01351287bbe6614e9
Annika <roederanni@gmx.de>
2017-12-20 17:58:27 +0100

    Mittwoch 18 Uhr Permission muss umgangen werden, alles andere klappt

--- app/src/main/java/com/example/madlo/mywaytest1/NavigationDrawer.java
+++ app/src/main/java/com/example/madlo/mywaytest1/NavigationDrawer.java
@@ -1,29 +1,67 @@
 package com.example.madlo.mywaytest1;
 
+import android.content.DialogInterface;
+import android.content.IntentSender;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.AndroidRuntimeException;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.SettingsClient;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 
 public class NavigationDrawer extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener, OnMapReadyCallback {
+        implements NavigationView.OnNavigationItemSelectedListener, OnMapReadyCallback, LocationListener, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
+    private static final int MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 222;
+    private static final String REQUESTING_LOCATION_UPDATES_KEY = "111";
     private GoogleMap mMap;
+    private LocationCallback mLocationCallback;
+    private Boolean mRequestingLocationUpdates;
+    private FusedLocationProviderClient mFusedLocationClient;
+    private LocationRequest mLocationRequest;
+    private String TAG = "Tag";
+    private GoogleApiClient mGoogleApiClient;
+    public static final String TAG2 = MapsActivity.class.getSimpleName();
+    private final static int CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -55,7 +93,94 @@ public class NavigationDrawer extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
         //Annika und Vicky
+
+        //Permission Check Vicky:  Berechtigung prüfen
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+
+
+        //Team Treehouse
+        mLocationRequest = LocationRequest.create()
+                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+                .setInterval(10 * 1000)
+                .setFastestInterval(1 * 1000);
     }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                    }
+
+                } else {
+                    Toast.makeText(getApplicationContext(), "This app requires permission to be granted", Toast.LENGTH_LONG).show();
+                    finish();
+                }
+
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+    }
+
+/** @Override public void onRequestPermissionsResult(int requestCode,
+String permissions[], int[] grantResults) {
+switch (requestCode) {
+case MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION: {
+// If request is cancelled, the result arrays are empty.
+if (grantResults.length > 0
+&& grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+Log.d(TAG, "Permission Granted");
+
+// permission was granted, yay! Do the
+// contacts-related task you need to do.
+
+} else {
+Log.d(TAG, "Permission denied");
+AlertDialog.Builder ADbuilder = new AlertDialog.Builder(this);
+ADbuilder.setMessage("This permission is important for the App to function properly.")
+.setTitle("Important permission required.")
+.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+@Override public void onClick(DialogInterface dialog, int id) {
+ActivityCompat.requestPermissions(NavigationDrawer.this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+}
+});
+ADbuilder.create();
+ADbuilder.show();
+
+// permission denied, boo! Disable the
+// functionality that depends on this permission.
+}
+
+}
+
+// other 'case' lines to check for other
+// permissions this app might request
+}
+}*/
+
+    /** LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
+     .addLocationRequest(mLocationRequest);
+
+
+
+     SettingsClient client = LocationServices.getSettingsClient(this);
+     Task<LocationSettingsResponse> task = client.checkLocationSettings(builder.build());
+     */
+
 
     @Override
     public void onBackPressed() {
@@ -117,14 +242,152 @@ public class NavigationDrawer extends AppCompatActivity
         return true;
     }
 
+    //*@Override
+
+    /**public void onMapReady(Location location) {
+     //mMap = location;
+     Log.d(TAG2, location.toString());
+     double currentLatitude = location.getLatitude();
+     double currentLongitude = location.getLongitude();
+
+     LatLng latLng = new LatLng(currentLatitude, currentLongitude);
+     mMap.addMarker(new MarkerOptions().position(new LatLng(0,0)).title("Marker"));
+
+     MarkerOptions options = new MarkerOptions()
+     .position(latLng)
+     .title("I am here!");
+     mMap.addMarker(options);
+     mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+
+
+
+
+
+
+     }*/
+    //Annika und Vicky
+    @Override
+    protected void onResume() {
+        super.onResume();
+        /**if (mRequestingLocationUpdates) {
+         startLocationUpdates();
+         }*/
+        //Team Treehouse
+        //setUpMapIfNeeded();
+        mGoogleApiClient.connect();
+    }
+
+    //TEam Treehouse
+    private void setUpMap() {
+        mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+    }
+
+    private void startLocationUpdates() {
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mFusedLocationClient.requestLocationUpdates(mLocationRequest,
+                mLocationCallback,
+                null /* Looper */);
+        mRequestingLocationUpdates = true;
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        /**stopLocationUpdates();*/
+        //Team Treehouse
+        if (mGoogleApiClient.isConnected()) {
+            LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
+            mGoogleApiClient.disconnect();
+        }
+    }
+
+
+    //Team Treehouse
+    @Override
+    public void onLocationChanged(Location location) {
+        handleNewLocation(location);
+
+    }
+
+    //Team Treehouse
+    private void handleNewLocation(Location location) {
+        Log.d(TAG2, location.toString());
+        double currentLatitude = location.getLatitude();
+        double currentLongitude = location.getLongitude();
+        LatLng latLng = new LatLng(currentLatitude, currentLongitude);
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+    }
+
+    //Team Treehouse
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        Log.i(TAG2, "Location services connected.");
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        //Team Treehouse
+        Location location = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        if (location == null) {
+            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+
+        } else {
+            handleNewLocation(location);
+        }
+        ;
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.i(TAG2, "Location services suspended. Please reconnect. ");
+    }
+
+    //Team Treehouse
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+        if (connectionResult.hasResolution()) {
+            try {
+                connectionResult.startResolutionForResult(this, CONNECTION_FAILURE_RESOLUTION_REQUEST);
+            } catch (IntentSender.SendIntentException e) {
+                e.printStackTrace();
+            }
+        } else {
+            Log.i(TAG2, "Location services connection failed with code " + connectionResult.getErrorMessage());
+        }
+    }
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mMap.setMyLocationEnabled(true);
+        }
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-    }
-    //Annika und Vicky
+
 }
